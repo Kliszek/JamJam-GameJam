4002166574da4d68c4f6c89c843ea8ee6d1eff16
Joachim Wałęga <32618387+Fan-of-Saints@users.noreply.github.com>
2022-03-05 18:29:20 +0100

    GameOverSplash, Pt. II
    Added new protips.

--- Assets/Scenes/ProtipDisplay.cs
+++ Assets/Scenes/ProtipDisplay.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class ProtipDisplay : MonoBehaviour
 {
-    private string[] protips = { "Just don't die", "If you shoot an enemy, they get killed", "Load your ultimate bar by shooting down enemies" };
+    private string[] protips = { "Try not to get shot next time", "If you dodge bullets fast enough, you won't get killed", "If you shoot an enemy, they immediately die", "Load your ultimate bar by shooting down enemies", "Make sure not to paste bullet movement into the player's script", "Pay attention to your mental health – stop playing when you're too tired" };
     public Text protipText;
 
     void Start()
