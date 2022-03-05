c70cbe022b5dd4c38052621efd50b2526ff9e497
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-03-05 17:16:28 +0100

    Added Ghost AI

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+//using Unity.AI.Navigation;
+
+public class Ghost : MonoBehaviour
+{
+
+    public float damage;
+    private Transform playerInstance;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInstance = GameManager.playerInstance.transform;
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = playerInstance.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player got hit!");
+            //GameManager.playerInstance.TakeDamage(damage);
+        }
+    }
+}
