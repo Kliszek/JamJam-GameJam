fe694334f4ae62c9274733bffdb590c6db536da3
wtrzeci <72412401+wtrzeci@users.noreply.github.com>
2022-03-13 19:22:59 +0100

    End screen Fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -77,7 +77,8 @@ public class GameManager : MonoBehaviour
 
     public void OnDie()
     {
-        Time.timeScale = 0.0f;
         SceneManager.LoadScene(2);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 }
